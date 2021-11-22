3ab3d2922cf08a98b202c20cb76fecc672c97ada
isrezaei <rezaeiism@yahoo.com>
2021-11-22 17:30:50 +0330

    Recenty Login , Add (Hello my dear friend) Component , and add New Style For One user login & handel style for recently options

--- src/LoginPage/LoginRecently/LoginRecently.jsx
+++ src/LoginPage/LoginRecently/LoginRecently.jsx
@@ -1,11 +1,12 @@
 import {useEffect , useState} from "react";
+import PleaseSigin from '../../Images/PleaseSigin.gif'
 import './LoginRecently.css'
 
 export default function LoginRecently ()
 {
 
     const [Recently , SetRecenty] = useState({
-        LocalUser : ['']
+        LocalUser : []
     })
 
     const [Rerander , SetRerander] = useState(false)
@@ -18,11 +19,11 @@ export default function LoginRecently ()
 
         const Local = JSON.parse(localStorage.getItem('New-User'))
 
-            SetRerander(false)
+        SetRerander(false)
 
-            Local && SetRecenty({
-                LocalUser : Local
-            })
+        Local && SetRecenty({
+            LocalUser : Local
+        })
 
 
     } , [Rerander])
@@ -48,21 +49,31 @@ export default function LoginRecently ()
             <div key={index}  className={'Card'}>
                 <img className={'Avatar'} alt={''}/>
                 <p className={'Name'}>{value.FirstName} {value.LastName}</p>
-                <p onClick={()=>{return ( RemoveLocal(value.FirstName) , SetRerander(true))}} className={'Activity'}>Remove User</p>
+                <p onClick={()=>{return ( RemoveLocal(value.FirstName) , SetRerander(true))}} className={'RemoveRecently'}>Remove User</p>
             </div>
         )
     })
 
 
+    const RecentlyLength = Recently.LocalUser.length === 0
 
+    console.log(RecentlyLength)
 
     return (
         <>
-            <h3 className={'UpperText'}>Recently Login..</h3>
+            {!RecentlyLength && <h3 className={'RecentlyLoginText'}>Recently Login ...</h3>}
 
-            <div className={'LoginRecently'}>
 
-                {ShowRecently}
+
+            <div className={Recently.LocalUser.length <= 1 ?  'LoginRecentlyForOnePerson' : 'LoginRecently'  }>
+
+                {!RecentlyLength  ? ShowRecently :
+                    <div className={'WellcomeMessage'}>
+                        <img className={'UserLogo'} src={PleaseSigin}/>
+                        <h4 className={'WellcomeText'}>Hello my dear friend Please login or create an account </h4>
+                    </div>
+                }
+
 
             </div>
 
