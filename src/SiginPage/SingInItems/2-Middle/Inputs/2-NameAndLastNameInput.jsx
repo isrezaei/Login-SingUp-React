985cb51b37a691c27958e4159f432f3c5ae49ba3
isrezaei <rezaeiism@yahoo.com>
2021-12-08 22:23:50 +0330

    Main branch Seve informations

--- src/SiginPage/SingInItems/2-Middle/Inputs/2-NameAndLastNameInput.jsx
+++ src/SiginPage/SingInItems/2-Middle/Inputs/2-NameAndLastNameInput.jsx
@@ -5,11 +5,13 @@ export default function NameAndLastNameInput ({InputName , InputLastName , SetLa
 
             <input  ref={InputName} type="text" placeholder='First name'
                     onFocus={()=> SetFirstName('.')}
+                    onClick={(e)=> SetFirstName(e.target.value)}
                     onBlur={(e)=> SetFirstName(e.target.value)}
                     onChange={(e)=> SetFirstName(e.target.value)}/>
 
             <input ref={InputLastName} type="text" placeholder='Last name'
                    onFocus={()=> SetLastName('.')}
+                   onClick={(e)=> SetLastName(e.target.value)}
                    onBlur={(e)=> SetLastName(e.target.value)}
                    onChange={(e)=> SetLastName(e.target.value)} />
         </div>
